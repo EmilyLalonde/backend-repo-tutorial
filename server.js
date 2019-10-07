7697e5680faea29cdc3b16abd65dbde9e9366b57
Emily <lalonde0902@gmail.com>
2019-10-07 11:43:33 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+
+app.set('port', process.env.PORT || 3001);
+app.locals.title = 'Seasons';
+app.locals.seasons = [
+  {id: 1, name: 'Spring', weather: 'moderate'},
+  {id: 2, name: 'Summer', weather: 'hot'},
+  {id: 3, name: 'Fall', weather: 'moderate'},
+  {id: 4, name: 'Winter', weather: 'cold'}
+]
+
+app.use(express.json());
+app.use(cors());
+app.listen(app.get('port'), () => {
+  console.log(`App is listening on port ${app.get('port')}`);
+});
+
+app.get('/', (request, response) => {
+  response.send('Seasons are wonderful!')
+})
+
+app.get('/api/v1/seasons', (request, response) => {
+  const { seasons } = app.locals
+  return response.status(200).json({ seasons })
+})
